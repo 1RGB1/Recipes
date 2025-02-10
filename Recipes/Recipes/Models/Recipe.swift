ca45da9c7fca22e191b39ff24f826285cd128c30
Ahmad Ragab <a.ragab2012@gmail.com>
2025-02-10 23:51:36 +0300

    feat: adding generic network layer

---
+++ Recipes/Recipes/Models/Recipe.swift
@@ -0,0 +1,25 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Ahmad Ragab on 10/02/2025.
+//
+
+struct Recipe: Identifiable, Decodable {
+    let id: Int
+    let name: String
+    let ingredients: [String]
+    let instructions: [String]
+    let prepTimeMinutes: Int
+    let cookTimeMinutes: Int
+    let servings: Int
+    let difficulty: String
+    let cuisine: String
+    let caloriesPerServing: Int
+    let tags: [String]
+    let userId: Int
+    let image: String
+    let rating: Double
+    let reviewCount: Int
+    let mealType: [String]
+}
