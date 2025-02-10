513dc0c053b72d0a6251aaa3c1c037de256cf567
Ahmad Ragab <a.ragab2012@gmail.com>
2025-02-10 22:51:56 +0300

    feat: adding the application to git

---
+++ Recipes/Recipes/App/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Ahmad Ragab on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
